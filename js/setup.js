3abae3c8f242eb7ff8fb50458d9d57c72a49f0ae
Anastasiushka <0503anastasia@gmail.com>
2018-09-05 20:49:20 +0300

    Работа над ошибками

--- js/setup.js
+++ js/setup.js
@@ -4,6 +4,8 @@ var WIZARD_NAMES = ['Иван ', 'Хуан Себастьян ', 'Мария ', 
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var wizardsAmount = 4;
+var wizards = [];
 
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
@@ -15,28 +17,13 @@ var getRandomI = function (min, max) {
   return randomI;
 };
 
-var wizards = [
-  {
+for (var i = 1; i <= wizardsAmount; i++) {
+  wizards.push({
     name: WIZARD_NAMES[getRandomI(0, WIZARD_NAMES.length - 1)] + WIZARD_SURNAMES[getRandomI(0, WIZARD_SURNAMES.length - 1)],
     coatColor: COAT_COLORS[getRandomI(0, COAT_COLORS.length - 1)],
     eyesColor: EYES_COLORS[getRandomI(0, EYES_COLORS.length - 1)]
-  },
-  {
-    name: WIZARD_NAMES[getRandomI(0, WIZARD_NAMES.length - 1)] + WIZARD_SURNAMES[getRandomI(0, WIZARD_SURNAMES.length - 1)],
-    coatColor: COAT_COLORS[getRandomI(0, COAT_COLORS.length - 1)],
-    eyesColor: EYES_COLORS[getRandomI(0, EYES_COLORS.length - 1)]
-  },
-  {
-    name: WIZARD_NAMES[getRandomI(0, WIZARD_NAMES.length - 1)] + WIZARD_SURNAMES[getRandomI(0, WIZARD_SURNAMES.length - 1)],
-    coatColor: COAT_COLORS[getRandomI(0, COAT_COLORS.length - 1)],
-    eyesColor: EYES_COLORS[getRandomI(0, EYES_COLORS.length - 1)]
-  },
-  {
-    name: WIZARD_NAMES[getRandomI(0, WIZARD_NAMES.length - 1)] + WIZARD_SURNAMES[getRandomI(0, WIZARD_SURNAMES.length - 1)],
-    coatColor: COAT_COLORS[getRandomI(0, COAT_COLORS.length - 1)],
-    eyesColor: EYES_COLORS[getRandomI(0, EYES_COLORS.length - 1)]
-  }
-];
+  });
+}
 
 var renderWizard = function (wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
