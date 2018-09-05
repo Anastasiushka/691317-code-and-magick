46e6a49225176fec7bb06ca2e0f4ccf52dd1d031
Anastasiushka <0503anastasia@gmail.com>
2018-09-05 20:52:07 +0300

    ///

--- js/setup.js
+++ js/setup.js
@@ -34,8 +34,8 @@ var renderWizard = function (wizard) {
 };
 
 var fragment = document.createDocumentFragment();
-for (var i = 0; i < wizards.length; i++) {
-  fragment.appendChild(renderWizard(wizards[i]));
+for (var j = 0; j < wizards.length; j++) {
+  fragment.appendChild(renderWizard(wizards[j]));
 }
 similarListElement.appendChild(fragment);
 
